730d04e1878640438c772e7ea47567b29374bfe5
rogerbf <roger@bergfalk.nu>
2017-01-05 17:20:50 +0100

    externally defined dependencies

--- source/index.js
+++ source/index.js
@@ -1,16 +1,4 @@
-import { spawn } from 'child_process'
-import getPorts from 'get-multiple-ports'
-import argsManager from 'args-manager'
-import logManager from 'tor-log'
-import hashPassword from 'tor-hashpassword'
-import getValidOptions from 'tor-torrc-options'
+import dependencies from './library/dependencies'
 import tor from './library/tor'
 
-export default tor.bind(null, {
-  spawn,
-  getPorts,
-  argsManager,
-  logManager,
-  hashPassword,
-  getValidOptions
-})
+export default tor.bind(null, dependencies)
