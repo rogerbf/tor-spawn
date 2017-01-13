6c99f67e07f63683357f4066d69aaa8b846dc11f
rogerbf <roger@bergfalk.nu>
2017-01-13 22:29:56 +0100

    100% coverage

--- source/library/handleControlPassword.test.js
+++ source/library/handleControlPassword.test.js
@@ -4,6 +4,48 @@ const hashPassword = jest.fn((pw) => {
   return Promise.resolve(`a_hash`)
 })
 
+const randomHexString = jest.fn(() => {
+  return Promise.resolve(`random_hex`)
+})
+
+const mockedDependencies = { hashPassword, randomHexString }
+
 test(`handleControlPassword`, () => {
   expect(typeof (handleControlPassword)).toBe(`function`)
+})
+
+test(`HashedControlPassword defined`, () => {
+  const tor = { options: { HashedControlPassword: `this_is_defined` } }
+  handleControlPassword(mockedDependencies, tor)
+  .then(tor => expect(tor).toBe(tor))
+  .catch(error => expect(error).toBeFalsy())
+})
+
+test(`controlPassword defined`, () => {
+  const tor = { controlPassword: `pass123`, options: {} }
+  handleControlPassword(mockedDependencies, tor)
+  .then(tor => expect(tor).toEqual(
+    {
+      ...tor,
+      options: {
+        HashedControlPassword: `a_hash`
+      }
+    }
+  ))
+  .catch(error => expect(error).toBeFalsy())
+})
+
+test(`no password defined`, () => {
+  const tor = { options: { } }
+  handleControlPassword(mockedDependencies, tor)
+  .then(tor => {
+    expect(tor).toEqual({
+      ...tor,
+      controlPassword: `random_hex`,
+      options: {
+        HashedControlPassword: `a_hash`
+      }
+    })
+  })
+  .catch(error => expect(error).toBeFalsy())
 })
